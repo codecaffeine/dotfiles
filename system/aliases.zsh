d7ea3dfc8d803ff3bf01c88ce72dde5b509f8645
Matt Thomas <matt@codecaffeine.com>
2012-04-07 10:49:39 -0400

    directory listing

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -3,8 +3,6 @@
 #   `brew install coreutils`
 if $(gls &>/dev/null)
 then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
+  alias ls="gls -lFh --color"
+  alias la="gls -lAFh --color"
 fi
