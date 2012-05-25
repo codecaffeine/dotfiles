0f2b5cb631bd325913a017e4dd86802f5f8eee15
Matt Thomas <matt@codecaffeine.com>
2012-05-25 10:06:39 -0400

    Fixed some stupid path issues

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,3 @@
-export PATH="./bin:/usr/local/git/bin:/usr/local/bin:/usr/local/sbin:$HOME/.rbenv/shims:$ZSH/bin:$PATH"
+export PATH="./bin:$HOME/.rbenv/bin:$HOME/.rbenv/shims:$ZSH/bin:/usr/local/git/bin:/usr/local/bin:/usr/local/sbin:$PATH"
 
 export MANPATH="/usr/local/git/man:/usr/local/man:/usr/local/mysql/man:$MANPATH"
