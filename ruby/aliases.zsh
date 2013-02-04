be147986a96ef45ee4f7299c508c4f9504c316c2
Matt Thomas <matt@codecaffeine.com>
2013-02-03 20:03:35 -0500

    getting rid of a bunch of stuff I don’t need

--- ruby/aliases.zsh
+++
@@ -1,7 +0,0 @@
-alias r='rbenv local 1.9.2-p290'
-
-alias sc='script/console'
-alias sg='script/generate'
-alias sd='script/destroy'
-
-alias migrate='rake db:migrate db:test:clone'
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,2 +1,1 @@
-alias reload!='. ~/.zshrc'
-alias fuck='curl rage.thewaffleshop.net'
+alias reload='source ~/.zshrc'
