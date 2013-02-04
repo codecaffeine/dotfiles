cb66d47a9fee807071c9ee65ea738712f3db607a
Matt Thomas <matt@codecaffeine.com>
2013-02-03 20:17:33 -0500

    Removing the todo info from prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -52,36 +52,12 @@ git_prompt(){
 	fi
 }
 
-# This keeps the number of todos always available the right hand side of my
-# command line. I filter it to only count those tagged as "+next", so it's more
-# of a motivation to clear out the list.
-todo(){
-  if $(which todo.sh &> /dev/null)
-  then
-    num=$(echo $(todo.sh ls +next | wc -l))
-    let todos=num-2
-    if [ $todos != 0 ]
-    then
-      echo "$todos"
-    else
-      echo ""
-    fi
-  else
-    echo ""
-  fi
-}
-
 directory_name(){
   echo "%n@%m %{$fg[cyan]%}%c%{$reset_color%}"
 }
 
 export PROMPT=$'[$(directory_name)$(git_prompt)]$ '
-set_prompt () {
-  export RPROMPT="%{$fg_bold[cyan]%}$(todo)%{$reset_color%}"
-}
 
 precmd() {
-  # title "zsh" "%m" "%55<...<%~"
   vcs_info
-  set_prompt
 }
